f9cef712992aaf1e3903ef4e034e89a75a5d0bb1
Maeharu-31 <rjc-430-cvg@e-net.nara.jp>
2025-05-02 04:55:05 +0000

    4_25

--- 4_25_8.c
+++ 4_25_8.c
@@ -3,8 +3,10 @@
 int main() {
     int i, j;
     for(i=1;i<=9;i++) {
-        for(j=1;j<=9;i++);
-            printf("%d\n", i*j);
+        for(j=1;j<=9;j++) {
+            printf("%3d", i * j);
+        }
+        printf("\n");
     }
     return 0;
 }
